7c73355cc3fa5c0cbcc0e447917e3d530934338a
Matheus Messora <tqi_mmessora@uolinc.com>
2014-07-23 18:43:16 -0300

    Code improvement

--- angulosoInterceptor.js
+++ angulosoInterceptor.js
@@ -9,78 +9,100 @@ anguloso.config(
 
                 return {
                     responseError: function(responseError) {
-
-                        var seconds = ((new Date() - responseError.config.anguloso.requestTime)/1000)%60;
-                        $("#" + id(responseError) + " .time").html(seconds + "s");
-                        $("#" + id(responseError) + " .status").html(responseError.status)
-                        $("#" + id(responseError)).addClass('error');
-                        $("#" + id(responseError)).addClass('error');
-                        var resultJSON = JSON.stringify(responseError.data, null , '\t');
-                        $("#" + id(responseError)).attr('title',resultJSON);
-
+                        callResponseError(responseError);
                         return responseError;
 
                     },
+
                     response: function(response) {
-
-                        var seconds = ((new Date() - response.config.anguloso.requestTime)/1000)%60;
-                        $("#" + id(response) + " .time").html(seconds + "s");
-                        $("#" + id(response) + " .status").html(response.status)
-                        $("#" + id(response)).delay(3000).addClass('success', {duration: 1000});
-
-                        var resultJSON = JSON.stringify(response.data, null , '\t');
-                        $("#" + id(response)).attr('title',resultJSON);
-
+                        callResponse(response);
                         return response;
                     },
+
                     request: function(request) {
-                        request.timeout = 2000;
-                        var id = makeid();
-                        request.anguloso = {
-                          "id": id,
-                          "url": request.url,
-                          "method": request.method,
-                          "requestTime": new Date()
-
-                        };
-                        $("#anguloso").append(new Array(
-                          "<div class='col-lg-24 request' id='" + id + "'>",
-                          "<div class='col-lg-10 url' style='float:left;'>/api/login</div>",
-                          "<div class='col-lg-4 status' style='float:left;'></div>",
-                          "<div class='col-lg-4 method' style='float:left;'>GET</div>",
-                          "<div class='col-lg-4 time' style='float:left;'></div>",
-                          "</div>"
-                          ).join(" "));
-                        $("#" + id + " .url").html(request.anguloso.url);
-                        $("#" + id + " .method").html(request.anguloso.method);
-
-
+                        callRequest(request);
                         return request;
                     }
-                } 
-                function makeid(){
-                    var text = "";
-                    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-
-                    for( var i=0; i < 5; i++ )
-                        text += possible.charAt(Math.floor(Math.random() * possible.length));
-
-                    return text;
-                }
-
-                function id(response){
-                    return response.config.anguloso.id;
-                }
-
-                
+                };
             } 
         );
     }
 );
-
 anguloso.run(function(){
-    
-    // Include div for anguloso
     $("<div id='anguloso'></div>").appendTo("body");
-
 });
+
+
+
+function callResponseError(responseError){
+    var seconds = ((new Date() - responseError.config.anguloso.requestTime)/1000)%60;
+    $("#" + id(responseError) + " .anTime").html(seconds + "s");
+    $("#" + id(responseError) + " .anStatus").html(responseError.status)
+    $("#" + id(responseError)).addClass('anError');
+    $("#" + id(responseError)).addClass('anError');
+    var resultJSON = JSON.stringify(responseError.data, null , '\t');
+    $("#" + id(responseError)).attr('title',resultJSON);
+}
+
+function callResponse(response){
+    var seconds = ((new Date() - response.config.anguloso.requestTime)/1000)%60;
+    $("#" + id(response) + " .anTime").html(seconds + "s");
+    $("#" + id(response) + " .anStatus").html(response.status)
+    $("#" + id(response)).delay(3000).addClass('anSuccess', {duration: 1000});
+
+    var resultJSON = JSON.stringify(response.data, null , '\t');
+    $("#" + id(response)).attr('title',resultJSON);
+
+    hide(id(response));
+}
+
+function callRequest(request){
+    var id = makeid();
+    request.anguloso = {
+      "id": id,
+      "url": request.url,
+      "method": request.method,
+      "requestTime": new Date()
+
+    };
+    $("#anguloso").append(new Array(
+      "<div class='col-lg-24 anRequest' id='" + id + "'>",
+      "<div class='col-lg-10 anUrl' style='float:left;'>/api/login</div>",
+      "<div class='col-lg-2 anStatus' style='float:left;'></div>",
+      "<div class='col-lg-2 anMethod' style='float:left;'>GET</div>",
+      "<div class='col-lg-2 anTime' style='float:left;'></div>",
+      "<div class='col-lg-2 anClose' style='float:left;'>CLOSE</div>",
+      "</div>"
+      ).join(" "));
+    $("#" + id + " .anUrl").html(request.anguloso.url);
+    $("#" + id + " .anMethod").html(request.anguloso.method);
+
+    $("#" + id + " .anClose").click(function() {
+        close(id);
+    });
+
+}
+
+function close(id){
+    $("#" + id).fadeOut(200);
+}
+
+function hide(id){
+    setTimeout(function() {
+        $("#" + id).fadeOut(1000);
+    },3000)
+};
+
+function id(response){
+    return response.config.anguloso.id;
+};
+
+function makeid(){
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for( var i=0; i < 5; i++ )
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+    return text;
+};
