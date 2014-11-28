a856f03bfbf38f23124ef8435d742eb559f7b1ff
Matheus Messora <tqi_mmessora@uolinc.com>
2014-11-28 16:15:28 -0200

    Consoler

--- angulosoInterceptor.js
+++ angulosoInterceptor.js
@@ -2,207 +2,208 @@
 var anguloso = angular.module('anguloso', []);
 
 anguloso.config(
-    function($provide) {
+    function ($provide) {
         $provide.factory('angulosoInterceptor',
 
-            function() {
+            function () {
 
                 return {
-                    responseError: function(responseError) {
+                    responseError: function (responseError) {
                         callResponseError(responseError);
                         return responseError;
 
                     },
 
-                    response: function(response) {
+                    response: function (response) {
                         callResponse(response);
                         return response;
                     },
 
-                    request: function(request) {
+                    request: function (request) {
                         callRequest(request);
                         return request;
                     }
                 };
-            } 
+            }
         );
     }
 );
-anguloso.run(function(){
+anguloso.run(function () {
     $(createAngulosoHTML()).appendTo("body");
 
     hide();
-    $("#angulosoHider").click(function() {
+    $("#angulosoHider").click(function () {
         showHide();
     });
 
-    PANDOX.CONSOLE.init();
+    LOGGER.init();
 
 
 });
 
-function createAngulosoHTML(){
+function createAngulosoHTML() {
     return new Array(
-    "<div id='angulosoHolder'>",
+        "<div id='angulosoHolder'>",
         "<div id='angulosoHider'><button id='angulosoHiderBtn' type='button' class='btn btn-primary btn-sm'>Anguloso &#9776;</button></div>",
-        "<div id='angulosoConsoler'><button id='angulosoConsoleBtn' type='button' class='btn btn-danger btn-sm'>Console &#9776;</button></div>",
+        "<div id='angulosoConsoler'><button id='angulosoConsoleBtn' type='button' class='btn btn-danger btn-sm'>Console &#10687;</button></div>",
         "<div id='anguloso'></div>",
-        "<div id='consoler'></div>",
-    "</div>",
+        "<div id='consolerHolder'></div>",
+        "</div>",
 
-    "").join(" ");
+        "").join(" ");
 }
 
-function callResponseError(responseError){
-    var seconds = ((new Date() - responseError.config.anguloso.requestTime)/1000)%60;
+function callResponseError(responseError) {
+    var seconds = ((new Date() - responseError.config.anguloso.requestTime) / 1000) % 60;
     $("#" + id(responseError) + " .anTime").html(seconds + "s");
     $("#" + id(responseError) + " .anStatus").html(responseError.status)
     $("#" + id(responseError)).addClass('anError');
     $("#" + id(responseError)).addClass('anError');
-    var resultJSON = JSON.stringify(responseError.data, null , '\t');
-    $("#" + id(responseError)).attr('title',resultJSON);
+    var resultJSON = JSON.stringify(responseError.data, null, '\t');
+    $("#" + id(responseError)).attr('title', resultJSON);
 
     addExtra(id(responseError), responseError);
 
-    console.log(responseError);
     show();
 }
 
-function callResponse(response){
-    var seconds = ((new Date() - response.config.anguloso.requestTime)/1000)%60;
+function callResponse(response) {
+    var seconds = ((new Date() - response.config.anguloso.requestTime) / 1000) % 60;
     $("#" + id(response) + " .anTime").html(seconds + "s");
     $("#" + id(response) + " .anStatus").html(response.status)
-    $("#" + id(response)).delay(3000).addClass('anSuccess', {duration: 1000});
+    $("#" + id(response)).delay(3000).addClass('anSuccess', {
+        duration: 1000
+    });
 
-    var resultJSON = JSON.stringify(response.data, null , '\t');
-    $("#" + id(response) + " .anUrl").attr('title',resultJSON);
+    var resultJSON = JSON.stringify(response.data, null, '\t');
+    $("#" + id(response) + " .anUrl").attr('title', resultJSON);
     addExtra(id(response), response);
 
-    console.log(response);
 
 }
 
 
 
-function callRequest(request){
+function callRequest(request) {
     var id = makeid();
     request.anguloso = {
-      "id": id,
-      "url": request.url,
-      "method": request.method,
-      "requestTime": new Date()
+        "id": id,
+        "url": request.url,
+        "method": request.method,
+        "requestTime": new Date()
 
     };
-    $("#anguloso").append(new Array(
-      "<div class='col-lg-24 anRequest' id='" + id + "'>",
-        "<div class='col-lg-1 anClose' style='float:left;'>",
+    if (request.url.search('.html') == -1) {
+        $("#anguloso").append(new Array(
+            "<div class='col-lg-24 anRequest' id='" + id + "'>",
+            "<div class='col-lg-1 anClose' style='float:left;'>",
             "<button type='button' class='btn btn-danger btn-xs'>",
             "<span class='glyphicon glyphicon-close'></span>X",
             "</button>",
-        "</div>",
-        "<div class='col-lg-1 anExpandBtn' style='float:left;'>",
+            "</div>",
+            "<div class='col-lg-1 anExpandBtn' style='float:left;'>",
             "<button type='button' class='btn btn-success btn-xs'>",
             "<span class='glyphicon glyphicon-close'></span>&#9776;",
             "</button>",
-        "</div>",
-      "<div class='col-lg-10 anUrl' style='float:left;'>/api/login</div>",
-      "<div class='col-lg-2 anStatus' style='float:left;'></div>",
-      "<div class='col-lg-2 anMethod' style='float:left;'></div>",
-      "<div class='col-lg-2 anTime' style='float:left;'></div>",
-      "").join(" "));
+            "</div>",
+            "<div class='col-lg-10 anUrl' style='float:left;'>/api/login</div>",
+            "<div class='col-lg-2 anStatus' style='float:left;'></div>",
+            "<div class='col-lg-2 anMethod' style='float:left;'></div>",
+            "<div class='col-lg-2 anTime' style='float:left;'></div>",
+            "").join(" "));
 
-    $("#" + id + " .anUrl").html(request.anguloso.url);
-    $("#" + id + " .anMethod").html(request.anguloso.method);
-
-
-    // Annimations
-    $("#" + id + " .anClose").click(function() {
-        close(id);
-    });
+        $("#" + id + " .anUrl").html(request.anguloso.url);
+        $("#" + id + " .anMethod").html(request.anguloso.method);
 
 
+        // Annimations
+        $("#" + id + " .anClose").click(function () {
+            close(id);
+        });
 
-    $("#" + id + " .anExpandBtn").click(function() {
-        expand(id);
-    });
 
 
+        $("#" + id + " .anExpandBtn").click(function () {
+            expand(id);
+        });
+
+    }
 }
 
-function addExtra(id, request){
+function addExtra(id, request) {
     var resultJSON = getPrettyJSON(request.data);
     $("#" + id).before("<div id='" + id + "_expand' class='col-lg-24 anExpand'><pre><code>" + resultJSON + "</code></pre></div>");
 }
 
-function getPrettyJSON(json){
+function getPrettyJSON(json) {
     // http://jsfiddle.net/unLSJ/
     var library = {};
 
     library.json = {
-        replacer: function(match, pIndent, pKey, pVal, pEnd) {
+        replacer: function (match, pIndent, pKey, pVal, pEnd) {
             var key = '<span class=json-key>';
             var val = '<span class=json-value>';
             var str = '<span class=json-string>';
             var r = pIndent || '';
             if (pKey)
-             r = r + key + pKey.replace(/[": ]/g, '') + '</span>: ';
+                r = r + key + pKey.replace(/[": ]/g, '') + '</span>: ';
             if (pVal)
-             r = r + (pVal[0] == '"' ? str : val) + pVal + '</span>';
+                r = r + (pVal[0] == '"' ? str : val) + pVal + '</span>';
             return r + (pEnd || '');
-            },
-        prettyPrint: function(obj) {
+        },
+        prettyPrint: function (obj) {
             var jsonLine = /^( *)("[\w]+": )?("[^"]*"|[\w.+-]*)?([,[{])?$/mg;
             return JSON.stringify(obj, null, 3)
-            .replace(/&/g, '&amp;').replace(/\\"/g, '&quot;')
-            .replace(/</g, '&lt;').replace(/>/g, '&gt;')
-            .replace(jsonLine, library.json.replacer);
-            }
+                .replace(/&/g, '&amp;').replace(/\\"/g, '&quot;')
+                .replace(/</g, '&lt;').replace(/>/g, '&gt;')
+                .replace(jsonLine, library.json.replacer);
+        }
     };
 
     return library.json.prettyPrint(json);
 
 }
 
-function expand(id){
-    if($("#" + id + "_expand").is(":hidden")){
-       $("#" + id + "_expand").slideDown();
-   }
-    else {
+function expand(id) {
+    if ($("#" + id + "_expand").is(":hidden")) {
+        $("#" + id + "_expand").slideDown();
+    } else {
         $("#" + id + "_expand").slideUp();
     }
 }
-function showHide(){
-    if($("#anguloso").is(":hidden")){
+
+function showHide() {
+    if ($("#anguloso").is(":hidden")) {
         show();
-    }else {
+    } else {
         hide();
     }
 }
 
-function show(){
+function show() {
     $("#anguloso").slideDown();
     $("#angulosoHiderBtn").html("Anguloso &#9776;");
 }
 
-function hide(){
+function hide() {
     $("#anguloso").slideUp();
     $("#angulosoHiderBtn").html("&#9776;");
 }
 
-function close(id){
+function close(id) {
     $("#" + id).fadeOut("fast");
     $("#" + id + "_expand").fadeOut("fast");
 }
 
-function id(response){
+function id(response) {
     return response.config.anguloso.id;
 };
 
-function makeid(){
+function makeid() {
     var text = "";
     var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
-    for( var i=0; i < 5; i++ )
+    for (var i = 0; i < 5; i++)
         text += possible.charAt(Math.floor(Math.random() * possible.length));
 
     return text;
--- pandox.js
+++ pandox.js
@@ -1,21 +1,65 @@
-var PANDOX = PANDOX || {};
+//var LOGGER = LOGGER || {};
 
 
 /*=====================================================================================================
- * Pandox CONSOLE Module
+ * LOGGER Module
  *======================================================================================================*/
-PANDOX.CONSOLE = function () {
+LOGGER = function () {
 
-	var init = function(){
-		console.log("PANDOX.CONSOLE.init");
+    var init = function () {
+        console.log("LOGGER.init");
 
+        $("#consolerHolder").html('<div class="col-lg-24" id="consoler"></div>');
+        
+        $("#angulosoConsoler").click(function () {
+            showHide();
+        });
+        log("LOGGER.init.success");
+    };
+    
+    function log(message){
+        appendLog("info", message);
+    };
+    
+    function error(message){
+        appendLog("error", message);
+    };
+    
+    var appendLog = function(level, message){
+        $("#consoler").append('<div class="row"><div class="col-lg-24 ' +getCssClassFromLogLevel(level)+ '">' + message + '</div></div>');
+    };
+    
+    var getCssClassFromLogLevel = function(level){
+        if(level === "info"){
+            return "text-primary";   
+        }
+        if(level === "error"){
+            return "text-danger";   
+        }
+    }
+                              
 
-		console.log("PANDOX.CONSOLE.init.success");
-	};
+    var showHide = function () {
+        if ($("#consolerHolder").is(":hidden")) {
+            show();
+        } else {
+           hide();
+        }
+    };
 
+    var show = function () {
+        $("#consolerHolder").slideDown();
+        $("#angulosoConsoleBtn").html("Console &#10687;");
+    }
 
+    var hide = function () {
+        $("#consolerHolder").slideUp();
+        $("#angulosoConsoleBtn").html("&#10687;");
+    }
 
-	return {
-		init: init
-	}
-};
+    return {
+        init: init,
+        log: log,
+        error: error
+    }
+}();
