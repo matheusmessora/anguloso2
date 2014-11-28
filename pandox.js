be6553eac653229dc3ce9c63b188d60be1a0e13d
Matheus Messora <tqi_mmessora@uolinc.com>
2014-11-28 13:09:16 -0200

    Consoler

---
+++ pandox.js
@@ -0,0 +1,21 @@
+var PANDOX = PANDOX || {};
+
+
+/*=====================================================================================================
+ * Pandox CONSOLE Module
+ *======================================================================================================*/
+PANDOX.CONSOLE = function () {
+
+	var init = function(){
+		console.log("PANDOX.CONSOLE.init");
+
+
+		console.log("PANDOX.CONSOLE.init.success");
+	};
+
+
+
+	return {
+		init: init
+	}
+};
